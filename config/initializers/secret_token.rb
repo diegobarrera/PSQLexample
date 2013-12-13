ddf950047b7d1a6a29cc20320bbadaf87d6c2541
Diego Barrera <dieg_goo@hotmail.com>
2013-12-12 22:01:04 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PSQLexample::Application.config.secret_token = 'fa3356dc490daaebefbc97b339a7ba08479ac68c05d42288ae90820f46d8d38e5c552c3d20f3500baec5bdd8ec6ec780194010ba896eabd4641e46cb3aca729f'
